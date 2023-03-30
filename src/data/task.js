a4615f7972d9440041a6985ceb6b740cc8ceef03
Daniel Perez <perdanny9@gmail.com>
2023-03-30 02:24:32 -0500

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,12 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Curso React",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Creeacion de tareas",
+  }
+];
